0d26b1c3d5dccc159c0c126d89e87f6b6e0c2858
System Administrator <root@sophie.intra.bpm.ch>
2020-06-16 13:55:12 +0200

    Added printNode.yml, generate_printnode_queue.py, added support for raw print, capabilities and multiuser

--- requestCapabilities.sh
+++ requestCapabilities.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -u  $(cat /etc/printNode.conf):  https://api.printnode.com/printers | jq '.[] | { printer: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
+API_KEYS=$(grep -i API_KEy /etc/cups/printNode.yml |  cut -d':' -f2-)
+
+while IFS= read -r line; do
+  curl -sS https://api.printnode.com/printers -u ${line}: | \
+    jq '.[] | { id: .id, name: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
+done <<< "${API_KEYS}"
