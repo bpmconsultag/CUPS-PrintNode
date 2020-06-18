5658b8f7900d51d88247375b59f4d5d905e4a3e6
System Administrator <root@sophie.intra.bpm.ch>
2020-06-18 16:53:35 +0200

    Bug fixes, adjustment of printNode title

--- requestCapabilities.sh
+++ requestCapabilities.sh
@@ -3,6 +3,8 @@
 API_KEYS=$(grep -i API_Key /etc/cups/printNode.yml |  cut -d':' -f2-)
 
 while IFS= read -r line; do
-  curl -sS https://api.printnode.com/printers -u ${line}: | \
-    jq '.[] | { id: .id, name: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
+  if [ -n "${line}" ] ; then
+    curl -sS https://api.printnode.com/printers -u ${line}: | \
+      jq '.[] | { id: .id, name: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
+  fi
 done <<< "${API_KEYS}"
