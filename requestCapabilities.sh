1a5d24eca64d0e1896d9d12042a3e87f074ad11e
System Administrator <root@sophie.intra.bpm.ch>
2020-08-24 12:01:19 +0200

    Error handling, when account without API Key are present

--- requestCapabilities.sh
+++ requestCapabilities.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-API_KEYS=$(grep -i API_Key /etc/cups/printNode.yml |  cut -d':' -f2-)
+API_KEYS=$(grep -i API_Key /etc/cups/printNode.yml | cut -d':' -f2- | grep -v "''" | grep -v '""')
 
 while IFS= read -r line; do
-  if [ -n "${line}" ] ; then
+  if [ -n "${line}" ] ; then 
     curl -sS https://api.printnode.com/printers -u ${line}: | \
-      jq '.[] | { id: .id, name: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
+      jq '.[] | { computer: .computer.name, id: .id, name: .name, InputTrays: .capabilities.bins, color: .capabilities.color, duplex: .capabilities.duplex }'
   fi
 done <<< "${API_KEYS}"
